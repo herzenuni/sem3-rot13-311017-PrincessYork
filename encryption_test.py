28154946fd481a2c9721853e37a20681a5832885
Egor Almikeev <york.git@gmail.com>
2017-11-11 19:32:21 +0300

    Update encryption_test.py

--- encryption_test.py
+++ encryption_test.py
@@ -1,5 +1,11 @@
+"""
+Единственная функция модуля позволяет протестировать взаимную обратность функций шифрования.
+Принимает две функции, и тестируемую строку.
+Возвращает True если декодирование закодированного исходного текста совпадает с исходным текстом.
+"""
+
 def cryption_function_testing(encrypt_function, decrypt_function, testing_string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"):
-	if encrypt_function(testing_string) == decrypt_function(encrypt_function(testing_string))
+	if testing_string == decrypt_function(encrypt_function(testing_string))
 		return True
 	else:
 		return False
